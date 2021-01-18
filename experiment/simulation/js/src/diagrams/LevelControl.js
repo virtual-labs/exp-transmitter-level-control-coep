9b76082962a047e06b97ccfb48933b527a692689
vaibhavkurhe <vaibhav.kurhe27@gmail.com>
2021-01-18 16:12:17 +0530

    leveldiagramChange

--- experiment/simulation/js/src/diagrams/LevelControl.js
+++ experiment/simulation/js/src/diagrams/LevelControl.js
@@ -2,12 +2,12 @@
 
 
 function LevelControl(){	
-	var w = 900;
-    var h = 500;
-paper = new Raphael(document.getElementById('canvas'), '100%', 700);
-paper.setViewBox(0,0,w,h,true);
-paper.setSize('100%', 700);
-	//paper = new Raphael(document.getElementById('canvas'), '100%', 700);
+	//var w = 900;
+   // var h = 500;
+//paper = new Raphael(document.getElementById('canvas'), '100%', 700);
+//paper.setViewBox(0,0,w,h,true);
+//paper.setSize('100%', 700);
+	paper = new Raphael(document.getElementById('canvas'), '100%', 700);
     //x = 310;
 	//y = 160;
 	
